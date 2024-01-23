eb663f49a8fa2e00694f36182eccf06e4ee6325a
Kenni Kollemorten <kenn7575@gmail.com>
2024-01-23 15:12:05 +0100

    Add project details and authors to project page

--- src/routes/projekter/[id]/+page.server.ts
+++ src/routes/projekter/[id]/+page.server.ts
@@ -1,5 +1,33 @@
 import type { PageServerLoad } from './$types';
+import { sql } from '@vercel/postgres';
+import type { DatabaseResponse, Project, ProjectAuthor, User } from '$lib/types';
+import { error } from '@sveltejs/kit';
 
-export const load = (async () => {
-	return {};
+export const load = (async ({ url }) => {
+	const id = url.pathname.split('/')[2];
+	if (!id) throw error(404, 'Der blev ikke fundet nogle projekter.');
+	if (typeof +id != 'number') throw error(404, 'Der blev ikke fundet nogle projekter.');
+
+	//get project
+	const { rows: projects } =
+		(await sql`SELECT * FROM projects WHERE id = ${id} LIMIT 1;`) as DatabaseResponse<Project>;
+	if (!projects || projects.length == 0) throw error(404, 'Der blev ikke fundet nogle projekter.');
+
+	//get authors
+	const { rows: authors } =
+		(await sql`SELECT * FROM project_authors WHERE project_id = ${id}`) as DatabaseResponse<ProjectAuthor>;
+
+	//attach authors to project
+	projects[0].authors = authors;
+
+	//for every author in project, attach user to author
+	for (const author of projects[0].authors) {
+		const { rows: users } =
+			(await sql`SELECT * FROM users WHERE id = ${author.user_id} LIMIT 1;`) as DatabaseResponse<User>;
+		author.user = users[0];
+	}
+
+	return {
+		project: projects[0]
+	};
 }) satisfies PageServerLoad;
